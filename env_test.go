f89b9e85bc95fc326b1361e6baaf2fe8a6b038b1
George Christou <christgf@gmail.com>
2022-10-03 23:35:01 +0300

    Tests refactoring

--- env_test.go
+++ env_test.go
@@ -8,256 +8,542 @@ import (
 )
 
 func TestString(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_STRING"
 
-	key = "TEST_STRING_VAR"
-	if got, want := env.String(key, "bar"), "bar"; got != want {
-		t.Errorf("String(%q): got %q, want %q", key, got, want)
+	if got, want := env.String(envKey, "foo"), "foo"; got != want {
+		t.Errorf("String(%q): got %v", envKey, got)
 	}
 
-	key = "TEST_STRING_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.String(key, "bar"), "foo"; got != want {
-		t.Errorf("String(%q): got %q, want %q", key, got, want)
+	t.Setenv(envKey, "bar")
+	if got, want := env.String(envKey, "foo"), "bar"; got != want {
+		t.Errorf("String(%q): got %q, want %q", envKey, got, want)
 	}
 }
 
 func TestBool(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_BOOL"
 
-	key = "TEST_BOOL_VAR"
-	if got := env.Bool(key, true); got != true {
-		t.Errorf("Bool(%q): got %v", key, got)
+	if got := env.Bool(envKey, true); got != true {
+		t.Errorf("Bool(%q): got %v", envKey, got)
 	}
 
-	key = "TEST_BOOL_VAR_TRUE"
-	t.Setenv(key, "true")
-	if got := env.Bool(key, false); got != true {
-		t.Errorf("Bool(%q): got %v", key, got)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  bool
+		wantValue bool
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  true,
+			wantValue: true,
+		},
+		{
+			name:      "Environment variable set to true",
+			envValue:  "true",
+			fallback:  false,
+			wantValue: true,
+		},
+		{
+			name:      "Environment variable set to false",
+			envValue:  "false",
+			fallback:  true,
+			wantValue: false,
+		},
+		{
+			name:      "Environment variable set to TRUE",
+			envValue:  "TRUE",
+			fallback:  false,
+			wantValue: true,
+		},
+		{
+			name:      "Environment variable set to FALSE",
+			envValue:  "FALSE",
+			fallback:  true,
+			wantValue: false,
+		},
+		{
+			name:      "Environment variable set to T",
+			envValue:  "T",
+			fallback:  false,
+			wantValue: true,
+		},
+		{
+			name:      "Environment variable set to F",
+			envValue:  "F",
+			fallback:  true,
+			wantValue: false,
+		},
+		{
+			name:      "Environment variable set to 1",
+			envValue:  "1",
+			fallback:  false,
+			wantValue: true,
+		},
+		{
+			name:      "Environment variable set to 0",
+			envValue:  "0",
+			fallback:  true,
+			wantValue: false,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  true,
+			wantValue: true,
+		},
 	}
-
-	key = "TEST_BOOL_VAR_ONE"
-	t.Setenv(key, "1")
-	if got := env.Bool(key, false); got != true {
-		t.Errorf("Bool(%q): got %v", key, got)
-	}
-
-	key = "TEST_BOOL_VAR_FALSE"
-	t.Setenv(key, "false")
-	if got := env.Bool(key, true); got != false {
-		t.Errorf("Bool(%q): got %v", key, got)
-	}
-
-	key = "TEST_BOOL_VAR_ZERO"
-	t.Setenv(key, "0")
-	if got := env.Bool(key, true); got != false {
-		t.Errorf("Bool(%q): got %v", key, got)
-	}
-
-	key = "TEST_BOOL_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got := env.Bool(key, true); got != true {
-		t.Errorf("Bool(%q): got %v", key, got)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Bool(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Bool(%q): got %v", envKey, got)
+			}
+		})
 	}
 }
 
 func TestInt(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_INT"
 
-	key = "TEST_INT_VAR"
-	if got, want := env.Int(key, 7), 7; got != want {
-		t.Errorf("Int(%q): got %d, want %d", key, got, want)
+	if got, want := env.Int(envKey, 42), 42; got != want {
+		t.Errorf("Int(%q): got %d, want: %d", envKey, got, want)
 	}
 
-	key = "TEST_INT_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Int(key, 7), 9; got != want {
-		t.Errorf("Int(%q): got %d, want %d", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  int
+		wantValue int
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  42,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is positive",
+			envValue:  "42",
+			fallback:  43,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-42",
+			fallback:  42,
+			wantValue: -42,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  42,
+			wantValue: 42,
+		},
 	}
-
-	key = "TEST_INT_VAR_NEGATIVE_NINE"
-	t.Setenv(key, "-9")
-	if got, want := env.Int(key, 7), -9; got != want {
-		t.Errorf("Int(%q): got %d, want %d", key, got, want)
-	}
-
-	key = "TEST_INT_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Int(key, 5), 5; got != want {
-		t.Errorf("Int(%q): got %d, want %d", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Int(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Int(%q): got %d, want: %d", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestInt64(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_INT64"
 
-	key = "TEST_INT64_VAR"
-	if got, want := env.Int64(key, 7), 7; got != int64(want) {
-		t.Errorf("Int64(%q): got %d, want %d", key, got, want)
+	if got, want := env.Int64(envKey, 42), int64(42); got != want {
+		t.Errorf("Int64(%q): got %d, want: %d", envKey, got, want)
 	}
 
-	key = "TEST_INT64_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Int64(key, 7), 9; got != int64(want) {
-		t.Errorf("Int64(%q): got %d, want %d", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  int64
+		wantValue int64
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  42,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is positive",
+			envValue:  "42",
+			fallback:  43,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-42",
+			fallback:  42,
+			wantValue: -42,
+		},
+		{
+			name:      "Environment variable is 64-bit positive",
+			envValue:  "9223372036854775807",
+			fallback:  42,
+			wantValue: 9223372036854775807,
+		},
+		{
+			name:      "Environment variable is 64-bit negative",
+			envValue:  "-9223372036854775808",
+			fallback:  42,
+			wantValue: -9223372036854775808,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  42,
+			wantValue: 42,
+		},
 	}
-
-	key = "TEST_INT64_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Int64(key, 5), 5; got != int64(want) {
-		t.Errorf("Int64(%q): got %d, want %d", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Int64(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Int64(%q): got %d, want: %d", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestUint(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_UINT"
 
-	key = "TEST_UINT_VAR"
-	if got, want := env.Uint(key, 7), uint(7); got != want {
-		t.Errorf("Uint(%q): got %d, want %d", key, got, want)
+	if got, want := env.Uint(envKey, 42), uint(42); got != want {
+		t.Errorf("Uint(%q): got %d, want: %d", envKey, got, want)
 	}
 
-	key = "TEST_UINT_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Uint(key, 7), uint(9); got != want {
-		t.Errorf("Uint(%q): got %d, want %d", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  uint
+		wantValue uint
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  42,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is positive",
+			envValue:  "42",
+			fallback:  43,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-42",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  42,
+			wantValue: 42,
+		},
 	}
-
-	key = "TEST_UINT_VAR_NEGATIVE_NINE"
-	t.Setenv(key, "-9")
-	if got, want := env.Uint(key, 7), uint(7); got != want {
-		t.Errorf("Uint(%q): got %d, want %d", key, got, want)
-	}
-
-	key = "TEST_UINT_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Uint(key, 5), uint(5); got != want {
-		t.Errorf("Uint(%q): got %d, want %d", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Uint(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Uint(%q): got %d, want: %d", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestUint64(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_UINT64"
 
-	key = "TEST_UINT64_VAR"
-	if got, want := env.Uint64(key, 7), 7; got != uint64(want) {
-		t.Errorf("Uint64(%q): got %d, want %d", key, got, want)
+	if got, want := env.Uint64(envKey, 42), uint64(42); got != want {
+		t.Errorf("Uint64(%q): got %d, want: %d", envKey, got, want)
 	}
 
-	key = "TEST_UINT64_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Uint64(key, 7), 9; got != uint64(want) {
-		t.Errorf("Uint64(%q): got %d, want %d", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  uint64
+		wantValue uint64
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  42,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is positive",
+			envValue:  "42",
+			fallback:  43,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-42",
+			fallback:  42,
+			wantValue: 42,
+		},
+		{
+			name:      "Environment variable is 64-bit positive",
+			envValue:  "9223372036854775807",
+			fallback:  42,
+			wantValue: 9223372036854775807,
+		},
+		{
+			name:      "Environment variable is 64-bit negative",
+			envValue:  "-9223372036854775808",
+			fallback:  9223372036854775807,
+			wantValue: 9223372036854775807,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  42,
+			wantValue: 42,
+		},
 	}
-
-	key = "TEST_UINT64_VAR_NEGATIVE_NINE"
-	t.Setenv(key, "-9")
-	if got, want := env.Uint64(key, 7), 7; got != uint64(want) {
-		t.Errorf("Uint64(%q): got %d, want %d", key, got, want)
-	}
-
-	key = "TEST_UINT64_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Uint64(key, 5), 5; got != uint64(want) {
-		t.Errorf("Uint64(%q): got %d, want %d", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Uint64(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Uint64(%q): got %d, want: %d", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestFloat32(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_FLOAT32"
 
-	key = "TEST_FLOAT32_VAR"
-	if got, want := env.Float32(key, 4.25), float32(4.25); got != want {
-		t.Errorf("Float32(%q): got %.2f, want %.2f", key, got, want)
+	if got, want := env.Float32(envKey, 42), float32(42); got != want {
+		t.Errorf("Float32(%q): got %.2f, want: %.2f", envKey, got, want)
 	}
 
-	key = "TEST_FLOAT32_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Float32(key, 7), float32(9.0); got != want {
-		t.Errorf("Float32(%q): got %.3f, want %.3f", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  float32
+		wantValue float32
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  4.2,
+			wantValue: 4.2,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  4.2,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is zero zero",
+			envValue:  "0.0",
+			fallback:  4.2,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is fraction",
+			envValue:  "0.2",
+			fallback:  0.3,
+			wantValue: 0.2,
+		},
+		{
+			name:      "Environment variable is negative fraction",
+			envValue:  "-0.2",
+			fallback:  0.3,
+			wantValue: -0.2,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-4.2",
+			fallback:  4.2,
+			wantValue: -4.2,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  4.2,
+			wantValue: 4.2,
+		},
 	}
-
-	key = "TEST_FLOAT32_VAR_TWO_POINT_NINE"
-	t.Setenv(key, "2.9")
-	if got, want := env.Float32(key, 7), float32(2.9); got != want {
-		t.Errorf("Float32(%q): got %.2f, want %.2f", key, got, want)
-	}
-
-	key = "TEST_FLOAT32_VAR_NEGATIVE_POINT_NINE"
-	t.Setenv(key, "-0.9")
-	if got, want := env.Float32(key, 7), float32(-0.9); got != want {
-		t.Errorf("Float32(%q): got %.2f, want %.2f", key, got, want)
-	}
-
-	key = "TEST_FLOAT32_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Float32(key, 5.25), float32(5.25); got != want {
-		t.Errorf("Float32(%q): got %.2f, want %.2f", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Float32(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Float32(%q): got %.2f, want: %.2f", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestFloat64(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_FLOAT64"
 
-	key = "TEST_FLOAT64_VAR"
-	if got, want := env.Float64(key, 4.25), 4.25; got != want {
-		t.Errorf("Float64(%q): got %.2f, want %.2f", key, got, want)
+	if got, want := env.Float64(envKey, 42), float64(42); got != want {
+		t.Errorf("Float64(%q): got %.2f, want: %.2f", envKey, got, want)
 	}
 
-	key = "TEST_FLOAT64_VAR_NINE"
-	t.Setenv(key, "9")
-	if got, want := env.Float64(key, 7), 9.0; got != want {
-		t.Errorf("Float64(%q): got %.3f, want %.3f", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  float64
+		wantValue float64
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  4.2,
+			wantValue: 4.2,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  4.2,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is zero zero",
+			envValue:  "0.0",
+			fallback:  4.2,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is fraction",
+			envValue:  "0.2",
+			fallback:  0.3,
+			wantValue: 0.2,
+		},
+		{
+			name:      "Environment variable is negative fraction",
+			envValue:  "-0.2",
+			fallback:  0.3,
+			wantValue: -0.2,
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-4.2",
+			fallback:  4.2,
+			wantValue: -4.2,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  4.2,
+			wantValue: 4.2,
+		},
 	}
-
-	key = "TEST_FLOAT64_VAR_TWO_POINT_NINE"
-	t.Setenv(key, "2.9")
-	if got, want := env.Float64(key, 7), 2.9; got != want {
-		t.Errorf("Float64(%q): got %.2f, want %.2f", key, got, want)
-	}
-
-	key = "TEST_FLOAT64_VAR_NEGATIVE_POINT_NINE"
-	t.Setenv(key, "-0.9")
-	if got, want := env.Float64(key, 7), -0.9; got != want {
-		t.Errorf("Float64(%q): got %.2f, want %.2f", key, got, want)
-	}
-
-	key = "TEST_FLOAT64_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Float64(key, 5.25), 5.25; got != want {
-		t.Errorf("Float64(%q): got %.2f, want %.2f", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Float64(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Float64(%q): got %.2f, want: %.2f", envKey, got, want)
+			}
+		})
 	}
 }
 
 func TestDuration(t *testing.T) {
-	var key string
+	const envKey = "ENV_TEST_FLOAT64"
 
-	key = "TEST_DURATION_VAR"
-	if got, want := env.Duration(key, 2*time.Hour), 2*time.Hour; got != want {
-		t.Errorf("Duration(%q): got %v, want %v", key, got, want)
+	if got, want := env.Duration(envKey, 2*time.Second), 2*time.Second; got != want {
+		t.Errorf("Duration(%q): got %v, want: %v", envKey, got, want)
 	}
 
-	key = "TEST_DURATION_VAR_TWO_HOURS"
-	t.Setenv(key, "2h")
-	if got, want := env.Duration(key, 3*time.Hour), 2*time.Hour; got != want {
-		t.Errorf("Duration(%q): got %v, want %v", key, got, want)
+	tests := []struct {
+		name      string
+		envValue  string
+		fallback  time.Duration
+		wantValue time.Duration
+	}{
+		{
+			name:      "Environment variable is empty",
+			envValue:  "",
+			fallback:  2 * time.Second,
+			wantValue: 2 * time.Second,
+		},
+		{
+			name:      "Environment variable is zero",
+			envValue:  "0",
+			fallback:  2 * time.Second,
+			wantValue: 0,
+		},
+		{
+			name:      "Environment variable is nano",
+			envValue:  "2ns",
+			fallback:  2 * time.Second,
+			wantValue: 2 * time.Nanosecond,
+		},
+		{
+			name:      "Environment variable is micro",
+			envValue:  "2μs",
+			fallback:  2 * time.Second,
+			wantValue: 2 * time.Microsecond,
+		},
+		{
+			name:      "Environment variable is millis",
+			envValue:  "2ms",
+			fallback:  2 * time.Second,
+			wantValue: 2 * time.Millisecond,
+		},
+		{
+			name:      "Environment variable is composite",
+			envValue:  "2h30m45s",
+			fallback:  2 * time.Hour,
+			wantValue: (2 * time.Hour) + (30 * time.Minute) + (45 * time.Second),
+		},
+		{
+			name:      "Environment variable is negative",
+			envValue:  "-2h30m",
+			fallback:  150 * time.Minute,
+			wantValue: -150 * time.Minute,
+		},
+		{
+			name:      "Environment variable is foobar",
+			envValue:  "foobar",
+			fallback:  2 * time.Second,
+			wantValue: 2 * time.Second,
+		},
 	}
-
-	key = "TEST_DURATION_VAR_TWO_AND_A_HALF_HOURS"
-	t.Setenv(key, "2h30m")
-	if got, want := env.Duration(key, 2*time.Hour), 150*time.Minute; got != want {
-		t.Errorf("Duration(%q): got %v, want %v", key, got, want)
-	}
-
-	key = "TEST_DURATION_VAR_NEGATIVE_UNITS"
-	t.Setenv(key, "-20ms")
-	if got, want := env.Duration(key, 20*time.Millisecond), -20*time.Millisecond; got != want {
-		t.Errorf("Duration(%q): got %v, want %v", key, got, want)
-	}
-
-	key = "TEST_DURATION_VAR_FOO"
-	t.Setenv(key, "foo")
-	if got, want := env.Duration(key, 180*time.Second), 3*time.Minute; got != want {
-		t.Errorf("Duration(%q): got %v, want %v", key, got, want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv(envKey, tt.envValue)
+			if got, want := env.Duration(envKey, tt.fallback), tt.wantValue; got != want {
+				t.Errorf("Duration(%q): got %v, want: %v", envKey, got, want)
+			}
+		})
 	}
 }
