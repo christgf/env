b881c04178e4c1234a1a04491810599c16d18446
George Christou <christgf@gmail.com>
2023-05-11 13:12:23 +0300

    Introducing type VarSet

--- env.go
+++ env.go
@@ -8,31 +8,30 @@ import (
 	"time"
 )
 
-// env represents an environment that maintains variables as key-value pairs of
-// strings. Its API is not yet stable therefore it is not yet public.
-type env struct {
+// VarSet represents a set of environment variables managed as key-value pairs.
+type VarSet struct {
 	prefix string
 }
 
-// SetPrefix makes this env prepend the value of prefix to every key before it
+// SetPrefix makes this VarSet prepend the value of prefix to every key before it
 // looks it up in the environment using String, StringVar, Bool, Int, et al. Use
 // the empty string to reset.
-func (e *env) SetPrefix(prefix string) {
-	e.prefix = prefix
+func (vs *VarSet) SetPrefix(prefix string) {
+	vs.prefix = prefix
 }
 
-// Prefix returns the prefix for this env, if any.
-func (e *env) Prefix() string {
-	return e.prefix
+// Prefix returns the prefix for this VarSet, if any.
+func (vs *VarSet) Prefix() string {
+	return vs.prefix
 }
 
-// lookup will apply the prefix for this env to the key provided and attempt to
+// lookup applies the prefix for this VarSet to the key provided and attempts to
 // retrieve the value of the corresponding environment variable. If the variable
 // is present in the environment the value is returned and the boolean is true.
 // Otherwise, the returned value will be empty and the boolean will be false.
-func (e *env) lookup(key string) (string, bool) {
-	if len(e.prefix) > 0 {
-		key = fmt.Sprintf("%s%s", e.prefix, key)
+func (vs *VarSet) lookup(key string) (string, bool) {
+	if len(vs.prefix) > 0 {
+		key = fmt.Sprintf("%s%s", vs.prefix, key)
 	}
 
 	return os.LookupEnv(key)
@@ -42,15 +41,15 @@ func (e *env) lookup(key string) (string, bool) {
 // the variable is present in the environment the value is returned and the
 // boolean is true. Otherwise, the returned value will be empty and the boolean
 // will be false.
-func (e *env) Lookup(key string) (string, bool) {
-	return e.lookup(key)
+func (vs *VarSet) Lookup(key string) (string, bool) {
+	return vs.lookup(key)
 }
 
 // String retrieves the value of the environment variable named by the key. If
 // the variable is present in the environment, its value (which may be empty) is
 // returned, otherwise fallback is returned.
-func (e *env) String(key string, fallback string) string {
-	value, ok := e.lookup(key)
+func (vs *VarSet) String(key string, fallback string) string {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -61,8 +60,8 @@ func (e *env) String(key string, fallback string) string {
 // Bool retrieves the value of the environment variable named by the key, parses
 // the value as a boolean, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
-func (e *env) Bool(key string, fallback bool) bool {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Bool(key string, fallback bool) bool {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -78,8 +77,8 @@ func (e *env) Bool(key string, fallback bool) bool {
 // Int retrieves the value of the environment variable named by the key, parses
 // the value as an integer, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
-func (e *env) Int(key string, fallback int) int {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Int(key string, fallback int) int {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -95,8 +94,8 @@ func (e *env) Int(key string, fallback int) int {
 // Int64 retrieves the value of the environment variable named by the key, parses
 // the value as a 64-bit integer, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
-func (e *env) Int64(key string, fallback int64) int64 {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Int64(key string, fallback int64) int64 {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -112,8 +111,8 @@ func (e *env) Int64(key string, fallback int64) int64 {
 // Uint retrieves the value of the environment variable named by the key, parses
 // the value as an unsigned integer, and returns the result. If the variable is
 // not present or its value cannot be parsed, fallback is returned.
-func (e *env) Uint(key string, fallback uint) uint {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Uint(key string, fallback uint) uint {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -129,8 +128,8 @@ func (e *env) Uint(key string, fallback uint) uint {
 // Uint64 retrieves the value of the environment variable named by the key,
 // parses the value as an unsigned 64-bit integer, and returns the result. If the
 // variable is not present or its value cannot be parsed, fallback is returned.
-func (e *env) Uint64(key string, fallback uint64) uint64 {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Uint64(key string, fallback uint64) uint64 {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -146,8 +145,8 @@ func (e *env) Uint64(key string, fallback uint64) uint64 {
 // Float32 retrieves the value of the environment variable named by the key,
 // parses the value as a floating-point number, and returns the result. If the
 // variable is not present or its value cannot be parsed, fallback is returned.
-func (e *env) Float32(key string, fallback float32) float32 {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Float32(key string, fallback float32) float32 {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -164,8 +163,8 @@ func (e *env) Float32(key string, fallback float32) float32 {
 // parses the value as a 64-bit floating-point number, and returns the result. If
 // the variable is not present or its value cannot be parsed, fallback is
 // returned.
-func (e *env) Float64(key string, fallback float64) float64 {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Float64(key string, fallback float64) float64 {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -181,8 +180,8 @@ func (e *env) Float64(key string, fallback float64) float64 {
 // Duration retrieves the value of the environment variable named by the key,
 // parses the value as time.Duration, and returns the result. If the variable is
 // not present or its value cannot be parsed, fallback is returned.
-func (e *env) Duration(key string, fallback time.Duration) time.Duration {
-	value, ok := e.lookup(key)
+func (vs *VarSet) Duration(key string, fallback time.Duration) time.Duration {
+	value, ok := vs.lookup(key)
 	if !ok {
 		return fallback
 	}
@@ -195,22 +194,20 @@ func (e *env) Duration(key string, fallback time.Duration) time.Duration {
 	return res
 }
 
-// osEnv is the default env. Top-level functions such as String, StringVar, Bool,
-// etc. are wrappers for the methods of osEnv.
-//
-// osEnv is not public, yet.
-var osEnv = &env{prefix: ""}
+// osVarSet is the default VarSet. Top-level functions such as String, StringVar,
+// Bool, etc. are wrappers for the methods of osVarSet.
+var osVarSet = &VarSet{prefix: ""}
 
-// SetPrefix makes this env prepend the value of prefix to every key before it
+// SetPrefix makes this VarSet prepend the value of prefix to every key before it
 // looks it up in the environment using String, StringVar, Bool, Int, et al. Use
 // the empty string to reset.
 func SetPrefix(prefix string) {
-	osEnv.SetPrefix(prefix)
+	osVarSet.SetPrefix(prefix)
 }
 
-// Prefix returns the prefix for the default env.
+// Prefix returns the prefix for the default VarSet.
 func Prefix() string {
-	return osEnv.Prefix()
+	return osVarSet.Prefix()
 }
 
 // Lookup retrieves the value of the environment variable named by the key. If
@@ -218,56 +215,56 @@ func Prefix() string {
 // boolean is true. Otherwise, the returned value will be empty and the boolean
 // will be false.
 func Lookup(key string) (string, bool) {
-	return osEnv.Lookup(key)
+	return osVarSet.Lookup(key)
 }
 
 // String retrieves the value of the environment variable named by the key. If
 // the variable is present in the environment, its value (which may be empty) is
 // returned, otherwise fallback is returned.
 func String(key string, fallback string) string {
-	return osEnv.String(key, fallback)
+	return osVarSet.String(key, fallback)
 }
 
 // Bool retrieves the value of the environment variable named by the key, parses
 // the value as a boolean, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
 func Bool(key string, fallback bool) bool {
-	return osEnv.Bool(key, fallback)
+	return osVarSet.Bool(key, fallback)
 }
 
 // Int retrieves the value of the environment variable named by the key, parses
 // the value as an integer, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
 func Int(key string, fallback int) int {
-	return osEnv.Int(key, fallback)
+	return osVarSet.Int(key, fallback)
 }
 
 // Int64 retrieves the value of the environment variable named by the key, parses
 // the value as a 64-bit integer, and returns the result. If the variable is not
 // present or its value cannot be parsed, fallback is returned.
 func Int64(key string, fallback int64) int64 {
-	return osEnv.Int64(key, fallback)
+	return osVarSet.Int64(key, fallback)
 }
 
 // Uint retrieves the value of the environment variable named by the key, parses
 // the value as an unsigned integer, and returns the result. If the variable is
 // not present or its value cannot be parsed, fallback is returned.
 func Uint(key string, fallback uint) uint {
-	return osEnv.Uint(key, fallback)
+	return osVarSet.Uint(key, fallback)
 }
 
 // Uint64 retrieves the value of the environment variable named by the key,
 // parses the value as an unsigned 64-bit integer, and returns the result. If the
 // variable is not present or its value cannot be parsed, fallback is returned.
 func Uint64(key string, fallback uint64) uint64 {
-	return osEnv.Uint64(key, fallback)
+	return osVarSet.Uint64(key, fallback)
 }
 
 // Float32 retrieves the value of the environment variable named by the key,
 // parses the value as a floating-point number, and returns the result. If the
 // variable is not present or its value cannot be parsed, fallback is returned.
 func Float32(key string, fallback float32) float32 {
-	return osEnv.Float32(key, fallback)
+	return osVarSet.Float32(key, fallback)
 }
 
 // Float64 retrieves the value of the environment variable named by the key,
@@ -275,74 +272,74 @@ func Float32(key string, fallback float32) float32 {
 // the variable is not present or its value cannot be parsed, fallback is
 // returned.
 func Float64(key string, fallback float64) float64 {
-	return osEnv.Float64(key, fallback)
+	return osVarSet.Float64(key, fallback)
 }
 
 // Duration retrieves the value of the environment variable named by the key,
 // parses the value as time.Duration, and returns the result. If the variable is
 // not present or its value cannot be parsed, fallback is returned.
 func Duration(key string, fallback time.Duration) time.Duration {
-	return osEnv.Duration(key, fallback)
+	return osVarSet.Duration(key, fallback)
 }
 
 // StringVar retrieves the value of the environment variable named by the key,
 // and stores the result into the variable pointed by p.
 func StringVar(p *string, key string, fallback string) {
-	*p = osEnv.String(key, fallback)
+	*p = osVarSet.String(key, fallback)
 }
 
 // BoolVar retrieves the value of the environment variable named by the key,
 // parses the value as a boolean, and stores the result into the variable pointed
 // by p.
 func BoolVar(p *bool, key string, fallback bool) {
-	*p = osEnv.Bool(key, fallback)
+	*p = osVarSet.Bool(key, fallback)
 }
 
 // IntVar retrieves the value of the environment variable named by the key,
 // parses the value as an integer, and stores the result into the variable
 // pointed by p.
 func IntVar(p *int, key string, fallback int) {
-	*p = osEnv.Int(key, fallback)
+	*p = osVarSet.Int(key, fallback)
 }
 
 // Int64Var retrieves the value of the environment variable named by the key,
 // parses the value as a 64-bit integer, and stores the result into the variable
 // pointed by p.
 func Int64Var(p *int64, key string, fallback int64) {
-	*p = osEnv.Int64(key, fallback)
+	*p = osVarSet.Int64(key, fallback)
 }
 
 // UintVar retrieves the value of the environment variable named by the key,
 // parses the value as an unsigned integer, and stores the result into the
 // variable pointed by p.
 func UintVar(p *uint, key string, fallback uint) {
-	*p = osEnv.Uint(key, fallback)
+	*p = osVarSet.Uint(key, fallback)
 }
 
 // Uint64Var retrieves the value of the environment variable named by the key,
 // parses the value as an unsigned 64-bit integer, and stores the result into the
 // variable pointed by p.
 func Uint64Var(p *uint64, key string, fallback uint64) {
-	*p = osEnv.Uint64(key, fallback)
+	*p = osVarSet.Uint64(key, fallback)
 }
 
 // Float32Var retrieves the value of the environment variable named by the key,
 // parses the value as a floating-point number, and stores the result into the
 // variable pointed by p.
 func Float32Var(p *float32, key string, fallback float32) {
-	*p = osEnv.Float32(key, fallback)
+	*p = osVarSet.Float32(key, fallback)
 }
 
 // Float64Var retrieves the value of the environment variable named by the key,
 // parses the value as a 64-bit floating-point number, and stores the result into
 // the variable pointed by p.
 func Float64Var(p *float64, key string, fallback float64) {
-	*p = osEnv.Float64(key, fallback)
+	*p = osVarSet.Float64(key, fallback)
 }
 
 // DurationVar retrieves the value of the environment variable named by the key,
 // parses the value as time.Duration, and stores the result into the variable
 // pointed by p.
 func DurationVar(p *time.Duration, key string, fallback time.Duration) {
-	*p = osEnv.Duration(key, fallback)
+	*p = osVarSet.Duration(key, fallback)
 }
